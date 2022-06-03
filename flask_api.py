ad9bc4e2960b2f8576f46d13505c417f91bbed8b
Muzi12345632 <muziwandilenko@gmail.com>
2022-06-03 22:58:56 +0200

    code for displaying random games

---
+++ flask_api.py
@@ -0,0 +1,27 @@
+import gym
+import numpy as np
+import time
+
+##TODO install mujoco_py module
+
+game= gym.envs.registry
+env= gym.make('FrozenLake8x8-v1')
+NUM_ACTIONS= env.action_space
+NUM_STATES= env.observation_space
+V= np.zeros_like([NUM_STATES])
+Pi= np.zeros_like([NUM_STATES], dtype=float)
+rew_total= 0
+observation= env.reset()
+
+
+#env.render()
+for _ in range (2):
+    action= env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    reward_total= rew_total+ reward
+    env.render()
+    env.reset()
+
+print(NUM_STATES)
+print(game)
+
