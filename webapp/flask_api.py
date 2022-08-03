c8f9615594fdd12ac02be5575ab57ea046176e2f
Muziwndile <muziwandilenko@gmail.com>
2022-08-03 11:08:43 +0200

    webapp folder bug sweep done!!!

--- webapp/flask_api.py
+++ webapp/flask_api.py
@@ -5,22 +5,21 @@ import time
 ##TODO install mujoco_py module
 
 game= gym.envs.registry
-env= gym.make('MountainCar-v0')
+env= gym.make('Blackjack-v1')
 NUM_ACTIONS= env.action_space
 NUM_STATES= env.observation_space
 V= np.zeros_like([NUM_STATES])
-Pi= np.zeros_like([NUM_STATES], dtype=float)
+Q= np.zeros_like([NUM_STATES], dtype=float)
 rew_total= 0
 observation= env.reset()
 
-env.reset()
-#env.render()
-for _ in range (2000):
+
+env.render()
+for _ in range (200):
     action= env.action_space.sample()
     observation, reward, done, info = env.step(action)
     reward_total= rew_total+ reward
     env.render()
-
-
+    env.reset()
 print(NUM_STATES)
 print(game)
